af6e5a556f626fe301c8c01b5141d96c5aecf3bf
Chienwang <R05228020@ntu.edu.tw>
2021-06-05 23:22:25 +0800

    updated favicon.ico

--- src/router/index.js
+++ src/router/index.js
@@ -6,28 +6,45 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
+    
     name: 'Home',
-    component: () => import("@/views/Home")
+    component: () => import("@/views/Home"),
+    meta: {
+      title: 'ECHO'}
   },
   {
     path: '/create',
     name: 'create',
-    component: () => import("@/views/create")
+    component: () => import("@/views/create"),
+    meta: {
+      title: 'ECHO'}
   },
   {
     path: '/manage',
     name: 'manage',
-    component: () => import("@/views/manage")
+    component: () => import("@/views/manage"),
+    meta: {
+      title: 'ECHO'}
   },
   {
     path: '/resume/:address',
     name: 'resume',
-    component: () => import("@/views/resume")
+    component: () => import("@/views/resume"),
+    meta: {
+      title: 'ECHO'}
   },
 ]
 
 const router = new VueRouter({
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+      document.title = to.meta.title
+  }
+  next();
+})
+
 
 export default router
